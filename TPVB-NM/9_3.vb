cfdec31fe625450d20042ae35cea368f8fc71ab7
natanmonchanin <natan.mo232005@gmail.com>
2024-10-03 14:19:26 +0200

    exercice 9.3

---
+++ TPVB-NM/9_3.vb
@@ -0,0 +1,19 @@
+﻿Module test
+    Sub Main()
+        Dim n As Integer
+        Do
+            Console.Write("Veuillez saisir un nombre:")
+            n = CInt(Console.ReadLine())
+        Loop While n <= 0
+
+        Console.WriteLine("Les {0} premiers nombres pairs supérieur à 0 sont :", n)
+        For i As Integer = 1 To n
+            Console.WriteLine(i * 2)
+        Next
+
+        Console.WriteLine("Appuyer sur une touche pour continuer")
+        Console.ReadKey()
+
+
+    End Sub
+End Module
